5c5b1542eb01189bed7cc2431448a1164e782626
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-05-04 01:53:26 +0300

    feat: added working lzw, lz77, lz78

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ import os
 from algorithms.huffman_coding import HuffmanTree
 from algorithms.LZW import LZWCompressor
 from algorithms.LZ78 import LZ78Compressor
+from algorithms.LZ77 import LZ77
 
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import (
@@ -26,35 +27,41 @@ class MainWindow(QMainWindow):
         self.central_widget = QWidget()
         self.layout = QVBoxLayout()
         self.layout.setContentsMargins(40, 30, 40, 30)
-        self.central_widget.setStyleSheet(
-            "background-color: #E8EEF2;"
+        self.central_widget.setStyleSheet("""
+            background-color: #E8EEF2;
+            """
         )
 
         self.name = QLabel("Data compressor")
         self.name.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.name.setStyleSheet(
-            "font-size: 35px;"
-            "color: #0E103D;"
-            "font-weight: 700;"
+        self.name.setStyleSheet("""
+            font-size: 35px;
+            color: #0E103D;
+            font-weight: 700;
+        """
         )
         self.layout.addWidget(self.name)
 
         self.caption = QLabel("Choose file for compression:")
         self.caption.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.caption.setStyleSheet(
-            "font-size: 25px;"
-            "color: black;"
-            "font-weight: 600;"
+            """
+            font-size: 25px;
+            color: black;"
+            font-weight: 600;
+            """
         )
         self.layout.addWidget(self.caption)
 
         self.pick_button = QPushButton("Pick a file")
         self.pick_button.setStyleSheet(
-            "font-size: 15px;"
-            "color: white;"
-            "font-weight: 500;"
-            "background-color: #0E103D;"
-            "border-radius: 10px"
+            """
+            font-size: 15px;
+            color: white;
+            font-weight: 500;
+            background-color: #0E103D;
+            border-radius: 10px;
+            """
         )
         self.pick_button.setFixedSize(QSize(400, 60))
         self.pick_button.clicked.connect(self.pick_file)
@@ -68,9 +75,11 @@ class MainWindow(QMainWindow):
         self.selected_file = None
         self.file_label = QLabel("No file selected")
         self.file_label.setStyleSheet(
-            "font-size: 15px;"
-            "color: black;"
-            "font-weight: 500;"
+            """
+            font-size: 15px;
+            color: black;
+            font-weight: 500;
+            """
         )
         self.file_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.selected_file_size = None
@@ -78,9 +87,11 @@ class MainWindow(QMainWindow):
 
         self.choose_alg = QLabel('Choose algorithm:')
         self.choose_alg.setStyleSheet(
-            "font-size: 20px;"
-            "color: black;"
-            "font-weight: 600;"
+            """
+            font-size: 20px;
+            color: black;
+            font-weight: 600;
+            """
         )
         self.layout.addWidget(self.choose_alg)
 
@@ -125,22 +136,20 @@ class MainWindow(QMainWindow):
         self.compress_file_size = 0
         self.compressed_size_label = QLabel("")
         self.compressed_size_label.setStyleSheet(
-            "font-size: 15px;"
-            "color: black;"
-            "font-weight: 500;"
+            """
+            font-size: 15px;
+            color: black;
+            font-weight: 500;
+            """
         )
         self.compressed_size_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.compressed_size_label)
-
-
 
         compress_button_layout = QHBoxLayout()
         compress_button_layout.addStretch()
         compress_button_layout.addWidget(self.compress_button)
         compress_button_layout.addStretch()
         self.layout.addLayout(compress_button_layout)
-
-
 
         self.decompress_button = QPushButton('Decompress')
         self.decompress_button.setStyleSheet(
@@ -161,9 +170,6 @@ class MainWindow(QMainWindow):
         decompress_button_layout.addStretch()
         self.layout.addLayout(decompress_button_layout)
 
-
-
-
         self.central_widget.setLayout(self.layout)
         self.setCentralWidget(self.central_widget)
 
@@ -180,7 +186,8 @@ class MainWindow(QMainWindow):
             self.selected_file_size = os.stat(self.selected_file).st_size
 
             acceptable_extensions = ['.jpg', '.tif', '.png', '.gif', \
-            '.flac', '.mp3', '.mpeg', '.txt', '.csv', '.json']
+            '.flac', '.mp3', '.flac', '.wav', '.mpeg', '.txt', \
+            '.csv', '.json', '.jpeg', '.bmp', '.avi', '.mp4']
             if file_ext not in acceptable_extensions:
                 QMessageBox.warning(self, "Unsupported File Format",\
                     f"This file{file_ext} is not supported.")
@@ -195,19 +202,19 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, "Error", "No file to compress, select it first")
             return
 
-        # lz77 and deflate to be added
-        algorithms_to_call = {'Huffman': HuffmanTree()}
+        algorithms_to_call = {'Huffman': HuffmanTree(), 'LZW': LZWCompressor(), \
+        'LZ78': LZ78Compressor(), 'LZ77': LZ77()}
 
         algorithm = self.algorithms_box.currentText()
-        # file_to_encode_ext = ''
         if algorithm in algorithms_to_call:
             file_to_encode = algorithms_to_call.get(algorithm)
             file_to_encode.compress_file(self.selected_file)
-            # file_to_encode_ext = os.path.splitext(self.selected_file)[1]
 
         self.compression_done = True
-        self.compress_file_size  = os.stat(f'./compressed_{algorithm.lower()}.bin').st_size
-        self.compressed_size_label.setText(f'Original size was: {round(self.selected_file_size / 1024, 2)} KB, now size is: {round(self.compress_file_size/ 1024, 2)} KB')
+        self.compress_file_size  = os.stat(f'compressed_{algorithm.lower()}.bin').st_size
+        self.compressed_size_label.setText(f'Original size was: {\
+        round(self.selected_file_size / 1024, 2)} KB, now size is: {\
+        round(self.compress_file_size/ 1024, 2)} KB')
         QMessageBox.information(self, "Success", f"File compressed using {algorithm}!")
 
     def decompress_file(self):
@@ -221,8 +228,8 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, "Error", "No file to compress, select it first")
             return
 
-        # lz77 and deflate to be added
-        algorithms_to_call = {'Huffman': HuffmanTree()}
+        algorithms_to_call = {'Huffman': HuffmanTree(), 'LZW': LZWCompressor(), \
+        'LZ78': LZ78Compressor(), 'LZ77': LZ77()}
         algorithm = self.algorithms_box.currentText()
 
         if algorithm in algorithms_to_call:
